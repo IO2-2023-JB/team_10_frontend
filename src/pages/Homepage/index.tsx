87c4ab4d71fece13842d8e1870aa7cbc4f91d3c7
Norbert Niziołek <nizioleque@gmail.com>
2023-04-20 22:22:32 +0200

    MW-170 Use VideoList on Homepage

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,18 +1,16 @@
 import { Stack, Typography } from '@mui/material';
-import { useNavigate } from 'react-router';
 import { useRecoilValue } from 'recoil';
 import { useUserDetails } from '../../api/user';
 import ContentSection from '../../components/layout/ContentSection';
 import PageLayout from '../../components/layout/PageLayout';
 import { userDetailsState } from '../../data/UserData';
-import { ProcessingProgress } from '../../types/VideoTypes';
 import { useAllVideos } from './../../api/video';
+import VideoList from '../../components/video/VideoList';
 
 function Homepage() {
   const { id: userId } = useRecoilValue(userDetailsState)!;
   const { data: userDetails, error, isLoading } = useUserDetails(userId);
   const { data: allVideos } = useAllVideos();
-  const navigate = useNavigate();
 
   return (
     <PageLayout>
@@ -23,31 +21,7 @@ function Homepage() {
               Dostępne filmy:
             </Typography>
             <Stack direction='column' spacing={3}>
-              {allVideos?.map((x) =>
-                x.processingProgress === ProcessingProgress.Ready ? (
-                  <Stack
-                    key={x.id}
-                    spacing={1}
-                    onClick={() => {
-                      navigate(`/video/${x.id}`);
-                    }}
-                  >
-                    <img
-                      height={200}
-                      width={300}
-                      style={{
-                        borderRadius: 10,
-                        borderColor: 'black',
-                        objectFit: 'cover',
-                      }}
-                      src={x.thumbnail ?? './public/placeholder_image.webp'}
-                    />
-                    <Typography fontSize={20}>{x.title}</Typography>
-                  </Stack>
-                ) : (
-                  <></>
-                )
-              )}
+              {allVideos !== undefined && <VideoList videos={allVideos} />}
             </Stack>
           </>
         )}
