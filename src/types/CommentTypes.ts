708eebd32ce77ba1ba9413eb554d72cf046656d0
Mateusz Zagórski <mateusz.zagorski2@gmail.com>
2023-06-05 17:43:50 +0200

    MW-249 Adjust comments to altered API endpoints

--- src/types/CommentTypes.ts
+++ src/types/CommentTypes.ts
@@ -1,8 +1,12 @@
-export interface GetComment {
+export interface CommentValues {
   id: string;
   authorId: string;
   content: string;
   avatarImage: string;
   nickname: string;
   hasResponses: false;
+}
+
+export interface GetComment {
+  comments: CommentValues[];
 }
