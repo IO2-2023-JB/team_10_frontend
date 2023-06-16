e7c3f1bc67577d24221bbf03477bfab2337fcce8
nowakmikolaj <nowak.mikolaj19@gmail.com>
2023-06-16 11:55:44 +0200

    Address review comments

--- src/pages/Upload/index.tsx
+++ src/pages/Upload/index.tsx
@@ -1,14 +1,27 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAdmin } from '../../api/user';
 import TabTitle from '../../components/TabTitle';
 import PageLayout from '../../components/layout/PageLayout';
+import { ROUTES } from '../../const';
 import { TabTitles } from '../../const/tab_titles';
 import VideoUploadForm from './VideoUploadForm';
 
 function Upload() {
+  const navigate = useNavigate();
+  const isAdmin = useAdmin();
+
+  useEffect(() => {
+    if (isAdmin) navigate(`/${ROUTES.HOMEPAGE}`, { replace: true });
+  });
+
   return (
-    <PageLayout>
-      <TabTitle title={TabTitles.Upload} />
-      <VideoUploadForm />
-    </PageLayout>
+    isAdmin !== undefined && (
+      <PageLayout>
+        <TabTitle title={TabTitles.Upload} />
+        <VideoUploadForm />
+      </PageLayout>
+    )
   );
 }
 
